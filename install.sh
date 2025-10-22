881c167c340a5cecfd90ed99b6d7f57277f40745
function3d <109918879+function3d@users.noreply.github.com>
2025-10-23 00:48:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ if [[ ! -d "${MOD}/root/mainsail_" ]]; then
   mv "${MOD}/root/mainsail" "${MOD}/root/mainsail_"
   echo "Existing Mainsail backed up to ${MOD}/root/mainsail_"
   chroot ${MOD} curl -s -L -k -o "/root/mainsail.zip" "https://github.com/function3d/mainsail/releases/download/v2.13.2-sms/mainsail.zip"
+  mkdir -p "${MOD}/root/mainsail"
   unzip -q "${MOD}/root/mainsail.zip" -d "${MOD}/root/mainsail"
   rm -f "${MOD}/root/mainsail.zip"
   sed -i 's/"port": null,/"port": "7125",/' "${MOD}/root/mainsail/config.json"
