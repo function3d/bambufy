8143343a1defa23d3fc33245bf362cb4cf3fbd8d
function3d <functionddd@gmail.com>
2026-06-10 21:18:54 +0500

    update.sh

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+CONF=/opt/config/printer.base.cfg
+awk '
+  /^\[stepper_z\]/ {
+    in_z = 1
+    print
+    next
+  }
+  /^\[/ {
+    in_z = 0
+    print
+    next
+  }
+  in_z && /^[[:space:]]*position_endstop[[:space:]]*:/ && !/^[[:space:]]*;/ {
+    print ";" $0
+    next
+  }
+  { print }
+' ${CONF} > /tmp/tmp && mv /tmp/tmp $CONF
